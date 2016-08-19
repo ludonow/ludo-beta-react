29ae1b1031b4b19e6ddf09cfdcf39f765194fea9
YongYuH <larry830326@hotmail.com>
2016-08-19 16:59:59 +0800

    [Add] linkage to create page on playground quick start

--- src/js/playground/QuickStart.js
+++ src/js/playground/QuickStart.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from "react-router";
+
+import Create from '../create/Create';
 
 export default class QuickStart extends React.Component {
     render() {
         return (
             <div className="grid-item">
-                 <div className="quick-start">
-                    <div className="quick-start__icon"></div>
-                 </div>
+                <Link to="Create" className="remove-hyperlink-style">
+                     <div className="quick-start">
+                        <div className="quick-start__icon"></div>
+                     </div>
+                  </Link>
             </div>
         );
     }
