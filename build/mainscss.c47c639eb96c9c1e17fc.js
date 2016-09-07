36d689f4f83b0f3d96aa7999651372c2582e0d6f
YongYuH <larry830326@hotmail.com>
2016-09-08 02:49:30 +0800

    [Add] layout of login page

--- build/mainscss.c47c639eb96c9c1e17fc.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,3,4],{0:function(n,o,c){n.exports=c(514)},514:function(n,o){}});
-//# sourceMappingURL=mainscss.c47c639eb96c9c1e17fc.js.map
--- src/js/app/Login.js
+++ src/js/app/Login.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-import imageLogo from '../../images/Ludo_logo.png';
+import logoImgPath from '../../images/Ludo_logo.png';
+import googleIcon from '../../images/login/google-icon.png';
+import twitterIcon from '../../images/login/twitter-icon.png';
+import facebookIcon from '../../images/login/facebook-icon.png';
 
 export default class Login extends React.Component {
     constructor() {
@@ -15,20 +18,40 @@ export default class Login extends React.Component {
         return ( 
             <div className="grid">
                 <div className="login-container">
-                    <div className="logo-container">
-                        <img src={imageLogo} className="logo-icon" />
+                    <div className="login-top-container">
+                        <div className="logo-container">
+                            <img src={logoImgPath} className="logo-icon" />
+                        </div>
+                        <div className="email">
+                            <input type="text" placeholder="email" />
+                        </div>
+                        <div className="password">
+                            <input type="text" placeholder="password" />
+                        </div>
+                        <button className="login">
+                            Log in
+                        </button>
+                        <button className="signup">
+                            Sign up
+                        </button>
                     </div>
-                    <div className="email">
-                        email
-                    </div>
-                    <div className="password">
-                        password
-                    </div>
-                    <div className="login">
-                        login
-                    </div>
-                    <div className="signup">
-                        signup
+                    <div className="login-bottom-container">
+                        <div className="other-login-instruction">
+                            <p>or</p>
+                            <br />
+                            <p>Log in via your favorite social media</p>
+                        </div>
+                        <div className="other-login-icon-container">
+                            <div className="twitter">
+                                <img src={twitterIcon} className="other-login-icon" />
+                            </div>
+                            <div className="google">
+                                <img src={googleIcon} className="other-login-icon" />
+                            </div>
+                            <div className="facebook">
+                                <img src={facebookIcon} className="other-login-icon" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
