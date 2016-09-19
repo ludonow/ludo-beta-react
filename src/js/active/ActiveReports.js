6f08e9f42dcdc55c2078429248f5e78cc695b086
YongYuH <larry830326@hotmail.com>
2016-09-19 20:59:07 +0800

    [Add] comment in report(only front-end)

--- src/js/active/ActiveReports.js
+++ src/js/active/ActiveReports.js
@@ -50,7 +50,7 @@ export default class ActiveReports extends React.Component {
                         <div className="report-text">
                             Report text
                         </div>
-                        <CommentBox />
+                        <CommentBox {...this.props} />
                     </div>
                 </div>
                 <div className="card player-container">
@@ -65,7 +65,7 @@ export default class ActiveReports extends React.Component {
                         <div className="report-text">
                             Report text
                         </div>
-                        <CommentBox />
+                        <CommentBox {...this.props} />
                     </div>
                 </div>
             </div>
@@ -74,19 +74,38 @@ export default class ActiveReports extends React.Component {
 };
 
 class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: []
+        };
+        this.updateMessageList = this.updateMessageList.bind(this);
+    }
+
+    updateMessageList(message) {
+        const { list } = this.state;
+        list.push(message);
+        this.setState(
+            Object.assign(this.state, {
+                list
+            })
+        )
+    }
+
     render() {
+        const { list } = this.state; 
         return (
             <div className="player-report-comment-box-container">
-                <CommentList />
-                <CommentForm />
+                <CommentList list={list} {...this.props} />
+                <CommentForm updateMessageList={this.updateMessageList} {...this.props} />
             </div>
         );
     }
 };
 
 class CommentForm extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             message: null
         };
@@ -102,7 +121,28 @@ class CommentForm extends React.Component {
                     message: event.target.value
                 })
             );
-            console.log('state message', this.state.message);
+            this.props.updateMessageList(state.message);
+
+            // const commentPost = {
+            //     'type': 'report',
+            //     'report_id': this.props.currentFormValue.ludo_id,
+            //     'content': state.message
+            // };
+            // axios.post('/apis/comment', commentPost)
+            // .then(response => {
+            //     if(response.data.status == '200') {
+            //         console.log('Successfully comment!');
+            //         console.log('response', response);
+            //     } else {
+            //         console.log('get after comment message post message from server: ', response.data.message);
+            //     }
+            // })
+            // .catch(error => {
+            //     console.log('get after comment message post error', error);
+            //     console.log('message from server: ', response.data.message);
+            // })
+
+            /* clear the text area of comment form */
             event.target.value = null;
             this.setState(
                 Object.assign(state, {
@@ -120,10 +160,27 @@ class CommentForm extends React.Component {
 }
 
 class CommentList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="comment-list">
-                commentlist
+                {
+                    this.props.list.map( (value, index) => {
+                        return (
+                            <div className="comment-container" key={`message-${index}`}>
+                                <div className="comment__avatar">
+                                    大頭
+                                </div>
+                                <div className="comment__message">
+                                    {value}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         );
     }
