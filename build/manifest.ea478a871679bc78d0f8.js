39a746b8696cbc28f66c9535e014e769061f83e1
YongYuH <larry830326@hotmail.com>
2016-10-02 23:43:14 +0800

    [Modify] friend page

--- build/manifest.ea478a871679bc78d0f8.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3,4],[]);
-//# sourceMappingURL=manifest.ea478a871679bc78d0f8.js.map
--- src/js/friend/FriendList.js
+++ src/js/friend/FriendList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Masonry from 'react-masonry-component';
 // import axios from '../axios-config';
-import config from '../axios-fakedata-config';
+// import config from '../axios-fakedata-config';
 
 const masonryOptions = {
     itemSelector: ".grid-item--friend",
@@ -13,27 +13,24 @@ export default class Friend extends React.Component {
     constructor() {
         super();
         this.state = { 
-            rawFriendList: [],
-            friendListWithCSS: []
+            rawFriendList: []
         };
     }
 
     componentDidMount() {
-        this.getFriendList();
+        // this.getFriendList();
     }
 
     getFriendList() {
-        const _this = this;
-
-        this.serverRequest = config.get('data/FriendData.json')
-            .then(function (response) {
-                _this.setState({
-                    rawFriendList: response.data
-                });
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
+        // this.serverRequest = config.get('data/FriendData.json')
+        //     .then(response => {
+        //         this.setState({
+        //             rawFriendList: response.data
+        //         });
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //     });
 
         // this.state.friendList = this.props.data.map( (data, index) => {
         //     return (
@@ -46,25 +43,23 @@ export default class Friend extends React.Component {
         // });
     }
 
-    addClass() {
-        this.state.friendListWithCSS = this.state.rawFriendList.map( (data, index) => {
-            return (
-                <div className="grid-item--friend" key={index}>
-                    <div className="friend-list__element">
-                        <img className="friend-list__icon" src={data.value} />
-                    </div>
-                </div>
-            );
-        });
-    }
-
     render() {
         this.addClass();
         return (
             <Masonry
                 className="grid"
                 options={masonryOptions}>
-                {this.state.friendListWithCSS}
+                {
+                    this.state.rawFriendList.map( (data, index) => {
+                        return (
+                            <div className="grid-item--friend" key={index}>
+                                <div className="friend-list__element">
+                                    <img className="friend-list__icon" src={data.value} />
+                                </div>
+                            </div>
+                        );
+                    })
+                }
             </Masonry>
         );
     }
