1fbe4aeb0569e0b79586a8082d5103702fc1ee2a
Po Chun, Lu <oceanus11034@gmail.com>
2017-01-20 23:06:21 +0800

    [Fix]  sidebarTemplate same class name

--- src/js/app/sidebar/SidebarTemplate.js
+++ src/js/app/sidebar/SidebarTemplate.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default class SidebarReadPlayground extends React.Component {
+export default class SidebarTemplate extends React.Component {
+
     render() {
         return (
             /* layout/_right-sidebar.scss */
-            <div className="right-sidebar-item__icon ludo-Template-icon"></div>
+            <div className = "right-sidebar-item__icon ludo-Template-icon" > < /div>
         );
     }
 }
