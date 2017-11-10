b801db3ec7c5eb0f44bad3d83f21124ee950461f
SeanGo <sean820117@gmail.com>
2017-11-10 14:29:20 +0800

    modify image of arrow icon

--- src/js/create/mobile-create-form/MobileCreateForm.js
+++ src/js/create/mobile-create-form/MobileCreateForm.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import { browserHistory } from 'react-router';
-import StepZilla from 'react-stepzilla';
+// import React, { Component } from 'react';
+// import { browserHistory } from 'react-router';
+// import StepZilla from 'react-stepzilla';
 
-import axios from '../../axios-config';
+// import axios from '../../axios-config';
 
-const steps = [
-  {name: 'Step 1', component: <Step1 />},
-  {name: 'Step 2', component: <Step2 />},
-  {name: 'Step 3', component: <Step3 />},
-  {name: 'Step 4', component: <Step4 />},
-  {name: 'Step 5', component: <Step5 />}
-];
+// const steps = [
+//   {name: 'Step 1', component: <Step1 />},
+//   {name: 'Step 2', component: <Step2 />},
+//   {name: 'Step 3', component: <Step3 />},
+//   {name: 'Step 4', component: <Step4 />},
+//   {name: 'Step 5', component: <Step5 />}
+// ];
 
-export default class MobileCreateForm extends Component {
-    constructor(props) {
-        super(props);
-    }
+// export default class MobileCreateForm extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-    render() {
-        return (
-            /* components/_form.scss */
-            <div className="">
-                <StepZilla steps={steps}/>
-            </div>
-        );
-    }
-}
+//     render() {
+//         return (
+//             /* components/_form.scss */
+//             <div className="">
+//                 <StepZilla steps={steps}/>
+//             </div>
+//         );
+//     }
+// }
