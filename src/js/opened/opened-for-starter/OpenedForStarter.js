223c3a768cd31db536fd7fa4eac3a931c2ad9900
YongYuH <larry830326@hotmail.com>
2017-11-30 23:32:34 +0800

    [fix] undefined variable issue

--- src/js/opened/opened-for-starter/OpenedForStarter.js
+++ src/js/opened/opened-for-starter/OpenedForStarter.js
@@ -7,15 +7,15 @@ import OpenedStarterForm from './OpenedStarterForm';
 
 /* components/_form.scss */
 const OpenedForStarter = (props) => (
-    <div className="form-and-list">
+    <div>
         <MediaQuery minDeviceWidth={768} className="form-and-report">
-            <OpenedStarterForm {...this.props} />
+            <OpenedStarterForm {...props} />
             <div className="form-ludo-list-container">
-                <LudoList {...this.props} />
+                <LudoList {...props} />
             </div>
         </MediaQuery>
         <MediaQuery maxDeviceWidth={768}>
-            <MobileOpenedLudo {...this.props} />
+            <MobileOpenedLudo {...props} starter />
         </MediaQuery>
     </div>
 );
