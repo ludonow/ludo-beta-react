aeab5e61fdd0d6c76e0f190205539e7b936a64f5
YongYuH <larry830326@hotmail.com>
2017-12-01 13:39:14 +0800

    [refactor] using pure function to refactor presentational components

--- src/js/create/Create.js
+++ src/js/create/Create.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MediaQuery from 'react-responsive';
 
 import CreateForm from './CreateForm';
 import MobileCreateCard from './mobile-create-card/MobileCreateCard';
 
-export default class Create extends Component {
-    constructor(props) {
-        super(props);
-    }
+/* components/_form.scss */
+const Create = (props) => (
+    <div>
+        <MediaQuery minDeviceWidth={768} className="form-and-list">
+            <CreateForm {...props} />
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={768}>
+            <MobileCreateCard {...props} />
+        </MediaQuery>
+    </div>
+);
 
-    render() {
-        return (
-            /* components/_form.scss */
-            <div>
-                <MediaQuery minDeviceWidth={768} className="form-and-list">
-                    <CreateForm {...this.props} />
-                </MediaQuery>
-                <MediaQuery maxDeviceWidth={768}>
-                    <MobileCreateCard {...this.props} />
-                </MediaQuery>
-            </div>
-        );
-    }
-}
+export default Create;
