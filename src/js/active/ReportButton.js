e94aa8a7e0ab6862ee4db9866c40e0f1016348cd
YongYuH <larry830326@hotmail.com>
2018-02-10 17:16:44 +0800

    [Style] modify font style of report button

--- src/js/active/ReportButton.js
+++ src/js/active/ReportButton.js
@@ -14,6 +14,8 @@ const Button = styled.button`
     border-top: none;
     border-width: 2px;
     box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+    font-family: "Exo", "Microsoft JhengHei";
+    font-size: 20px;
     height: 41px;
     justify-content: center;
     margin: 0 auto;
