367bd9c5135e2e79384d6e2867f8d88b4958d712
SeanGo <sean820117@gmail.com>
2018-02-24 19:24:01 +0800

    13232

--- src/js/baseurl-config.js
+++ src/js/baseurl-config.js
@@ -1,7 +1,7 @@
 import { createHistory } from 'history';
 import { useRouterHistory } from 'react-router';
 
-// modify test base url here
+// modify test1 base url here
 const browserHistory = useRouterHistory(createHistory)({
     basename: "/"
 });
