f709b500d0e8d159b8c2890276d295f5caf0c18c
YongYuH <larry830326@hotmail.com>
2018-03-03 23:24:11 +0800

    [Add] content of bind messenger

--- src/js/Bind/index.js
+++ src/js/Bind/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Dialog from 'material-ui/Dialog';
 
-const Bind = props => {
-    return (
-        <div>
-            Bind Page
-        </div>
-    );
-};
+import StepperCloseIcon from '../components/StepperCloseIcon';
+
+class Bind extends Component {
+    constructor() {
+        super();
+        this.state = {
+            open: true,
+        };
+        this.handleCloseClick = this.handleCloseClick.bind(this);
+    }
+
+    handleCloseClick() {
+        this.setState({
+            open: false,
+        })
+    }
+
+    render() {
+        const {
+            open
+        } = this.state;
+
+        return (
+            <div>
+                <Dialog
+                    onRequestClose={this.handleCloseClick}
+                    open={open}
+                    title="享受更好的平台體驗！"
+                >
+                    <StepperCloseIcon handleCloseClick={this.handleCloseClick} />
+                    點擊以下按鈕連結 Messenger
+                    確認綁定帳號
+                    LUDO 團隊‧創新開發了由 Messenger AI 導向的新功能，致力於優化使用者跨裝置便利性，歡迎您隨時反應使用上的經驗，我們會儘速優化及更新！
+                </Dialog>
+            </div>
+        );
+    }
+}
 
 Bind.propTypes = {
 };
