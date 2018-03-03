bf4869820104534406cb113095c1ae9172814918
YongYuH <larry830326@hotmail.com>
2018-03-03 17:26:43 +0800

    [Add] Bind page

---
+++ src/js/Bind/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Bind = props => {
+    return (
+        <div>
+            Bind Page
+        </div>
+    );
+};
+
+Bind.propTypes = {
+};
+
+export default Bind;
